3301ff99e4c8a8fc5b40cc1aeabac46729bd5dda
FraConv <francesco16convertini@gmail.com>
2024-07-05 17:01:57 +0200

    Policy

--- src/assets/Components/Footer.jsx
+++ src/assets/Components/Footer.jsx
@@ -6,6 +6,8 @@ function Footer(){
     
 <footer className=" w-[100%] h-24  left-0 mt-[500px] absolute bg-[#1b4455] z-40">
 <h6 className="min-[1400px]:ml-[-1410px] scale-90 mt-[36px] ml-[-110px] text-[14px] xl:ml-[-900px] xl:text-lg md:ml-[-400px] md:text-[19px]">Copyright © 2023 MimmoSoftweb</h6>
+<a className=" text-white hover:text-[#f9b65d] text-[14px] ml-[-140px] pt-[60px]  md:ml-[-505px] xl:ml-[-990px] min-[1400px]:ml-[-1503px] " href="https://www.iubenda.com/privacy-policy/13346282">Privacy Policy</a>
+<a className=" text-white hover:text-[#f9b65d] text-[14px] ml-[-190px] pt-[60px]" href="https://www.iubenda.com/privacy-policy/13346282/cookie-policy">Cookie Policy</a>
 </footer>
 
 
--- src/assets/Components/Raspberry.jsx
+++ src/assets/Components/Raspberry.jsx
@@ -36,4 +36,6 @@ function Raspberry(){
     )
     
     }
+
+    
     export default Raspberry
