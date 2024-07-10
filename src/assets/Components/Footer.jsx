4049f3c11012a84ade359e09e82c10794b9e82d4
FraConv <francesco16convertini@gmail.com>
2024-07-10 23:55:30 +0200

    m3

--- src/assets/Components/Footer.jsx
+++ src/assets/Components/Footer.jsx
@@ -4,7 +4,7 @@ function Footer(){
     
     <>
     
-<footer className=" w-[100%] h-24 xl:mt-[304px] min-[430px]:mt-[370px] min-[1400px]:mt-[390px]  left-0 min-[414px]:mt-[355px] min-[412px]:mt-[360px] md:mt-[500px] min-[390px]:mt-[330px] absolute bg-[#1b4455] z-40 min-[375px]:mt-[240px]">
+<footer className=" w-[100%] h-24 xl:mt-[303px] min-[430px]:mt-[370px] min-[1400px]:mt-[390px]  left-0 min-[414px]:mt-[355px] min-[412px]:mt-[360px] md:mt-[500px] min-[390px]:mt-[330px] absolute bg-[#1b4455] z-40 min-[375px]:mt-[240px]">
 <h6 className="text-[white] min-[1400px]:ml-[-1410px] scale-90 mt-[36px] ml-[-110px] text-[14px] xl:ml-[-900px] xl:text-lg md:ml-[-400px] md:text-[19px]">Copyright © 2023 MimmoSoftweb</h6>
 <a className=" text-white hover:text-[#f9b65d] text-[14px] ml-[-140px] pt-[60px]  md:ml-[-505px] xl:ml-[-990px] min-[1400px]:ml-[-1503px] " href="https://www.iubenda.com/privacy-policy/13346282">Privacy Policy</a>
 <a className=" text-white hover:text-[#f9b65d] text-[14px] ml-[-190px] pt-[60px]" href="https://www.iubenda.com/privacy-policy/13346282/cookie-policy">Cookie Policy</a>
