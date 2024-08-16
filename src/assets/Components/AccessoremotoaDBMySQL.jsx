d7d1ab6c5b8609aefa362f7b5b44ff118d918b1e
FraConv <francesco16convertini@gmail.com>
2024-08-16 15:37:39 +0200

    fine 2

--- src/assets/Components/AccessoremotoaDBMySQL.jsx
+++ src/assets/Components/AccessoremotoaDBMySQL.jsx
@@ -152,7 +152,7 @@ if __name__ == '__main__':
 
 <footer className=" w-[100%] h-24  left-0 mt-[30px] absolute bg-[#1b4455] z-40">
 <h6 className=" text-[white] min-[1400px]:ml-[-1410px] scale-90 mt-[36px] ml-[-110px] xl:ml-[-900px] xl:text-lg md:ml-[-400px] md:text-[19px]">Copyright © 2023 MimmoSoftweb</h6>
-<a className=" text-white hover:text-[#f9b65d] text-[14px] ml-[-170px] pt-[60px]  md:ml-[-510px] xl:ml-[-996px] min-[1400px]:ml-[-1506px]" href="https://www.iubenda.com/privacy-policy/13346282">Privacy Policy</a>
+<a className=" text-white hover:text-[#f9b65d] text-[14px] ml-[-174px] pt-[60px]  md:ml-[-510px] xl:ml-[-996px] min-[1400px]:ml-[-1506px]" href="https://www.iubenda.com/privacy-policy/13346282">Privacy Policy</a>
 <a className=" text-white hover:text-[#f9b65d] text-[14px] ml-[-190px] pt-[60px]" href="https://www.iubenda.com/privacy-policy/13346282/cookie-policy">Cookie Policy</a>
 </footer>
 
--- src/assets/Components/Web ServerconRaspberry.jsx
+++ src/assets/Components/Web ServerconRaspberry.jsx
@@ -344,7 +344,7 @@ BENE questo è tutto, spero che la procedura sia stata utile e se avete un Modem
 
 <footer className=" w-[100%] h-24  left-0 mt-[30px] absolute bg-[#1b4455] z-40">
 <h6 className="text-[white] min-[1400px]:ml-[-1410px] scale-90 mt-[36px] ml-[-110px] xl:ml-[-900px] xl:text-lg md:ml-[-400px] md:text-[19px]">Copyright © 2023 MimmoSoftweb</h6>
-<a className=" text-white hover:text-[#f9b65d] text-[14px] ml-[-170px] md:ml-[-510px] pt-[60px] xl:ml-[-996px] min-[1400px]:ml-[-1506px]" href="https://www.iubenda.com/privacy-policy/13346282">Privacy Policy</a>
+<a className=" text-white hover:text-[#f9b65d] text-[14px] ml-[-174px] md:ml-[-510px] pt-[60px] xl:ml-[-996px] min-[1400px]:ml-[-1506px]" href="https://www.iubenda.com/privacy-policy/13346282">Privacy Policy</a>
 <a className=" text-white hover:text-[#f9b65d] text-[14px] ml-[-190px] pt-[60px]" href="https://www.iubenda.com/privacy-policy/13346282/cookie-policy">Cookie Policy</a>
 </footer>
 
